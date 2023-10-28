3c8742663602ddd0d7fd900567ade32ae9853b7b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-28 16:16:45 +0200

    Added GCC to the flake.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "A very basic flake";
+    description = "grompt";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -32,6 +32,7 @@
                 nativeBuildInputs = with pkgs; [
                     pkg-config
                     openssl
+                    gcc
                 ];
             };
 
@@ -42,6 +43,7 @@
                     rust-analyzer
                     pkg-config
                     openssl
+                    gcc
                 ];
             };
         });
